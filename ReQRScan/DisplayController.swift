487907aae270777aa9649b4bde68bd3ee588910f
Taichi Kato <taichikato@LAB-N376.local>
2017-06-27 22:03:24 +0900

    Added QR Code display

--- ReQRScan/DisplayController.swift
+++ ReQRScan/DisplayController.swift
@@ -7,24 +7,82 @@
 //
 
 import UIKit
-
+import RxSwift
+import RxCocoa
 class DisplayController: UIViewController {
-
+    @IBOutlet weak var retrieveButton: UIButton!
+    private let disposebag = DisposeBag()
+    private var observable: Observable<String?>? = nil
+    @IBOutlet weak var close: UIButton!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var QRString: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        var qrCode = QRCode("Hello TeamLab!")
-        qrCode?.size = CGSize(width: 1000, height: 1000)
-        imageView.image = qrCode?.image
         // Do any additional setup after loading the view.
+        setUpQR()
+        setUpObservable()
+        bind()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
+    private func setUpObservable() {
+        QRString.text = "hello!"
+        observable = QRString.rx.text.asObservable()
+        observable?.subscribe(onNext: {(input: String?) in
+                self.setUpQR(string: input!)
+            })
+    }
+    private func setUpQR(string: String = "hello!") {
+        var qrCode = QRCode(string)
+        qrCode?.size = CGSize(width: 1000, height: 1000)
+        imageView.image = qrCode?.image
+    }
+    private func bind(){
+        let tapGesture = UITapGestureRecognizer()
+        tapGesture.rx.event
+            .subscribe(onNext: {tapGesture in
+                self.view.endEditing(true)
+                return
+            }).addDisposableTo(disposebag)
+        view.addGestureRecognizer(tapGesture)
+        
+        close.rx.tap
+            .subscribe(onNext: {[unowned self] in
+                self.dismiss(animated: true, completion: nil)
+            }).addDisposableTo(disposebag)
+        retrieve.rx.tap
+            .subscribe(onNext: {
+               let _ = mockAPI()
+                return
+            })
+    }
+    private func mockAPI(){
+        sleep(2)
+        return QRInfo()
+    }
+    class QRInfo{
+        let typeCode: String?
+        let price: String?
+        let companyCode = "0000"
+        let expiryDate : String?
+        let outletCode : String?
+        let others : String?
+        init() {
+            typeCode = "PPP"
+            price = "0100"
+            companyCode = "0000"
+            expiryDate = "170904"
+            outletCode = "TTTT"
+            others = "XXXXXX"
+        }
+        func qrString() -> String {
+            guard let string = (typeCode? + price? + companyCode + expiryDate? + outletCode? + others?) else {return ""}
+            return string
+        }
+    }
     /*
     // MARK: - Navigation
 
